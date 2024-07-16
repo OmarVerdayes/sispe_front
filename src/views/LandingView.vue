d0b6272dcc4741d72530408a57818b684d713949
Yexzari <20203tn141@utez.edu.mx>
2024-07-16 08:13:22 -0600

    landing creation

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -1,223 +1,112 @@
 <template>
-    <div class="main-container">
-        <div id="carousel_banner">
-            <carousel-3d :width="900" :height="450" :display="3" :controls-visible="true" :controls-prev-html="'❮'"
-                :controls-next-html="'❯'" :autoplay="true" :autoplay-timeout="5000" :autoplay-hover-pause="true"
-                :border="0">
-                <slide v-for="(slide, i) in dataCarousel" :index="i" :key="i" class="slide">
-                    <template slot-scope="{ index, isCurrent, leftIndex, rightIndex }">
-                        <img :data-index="index"
-                            :class="{ current: isCurrent, onLeft: (leftIndex >= 0), onRight: (rightIndex >= 0) }"
-                            :src="slide.front_page" >
-                    </template>
-                </slide>
-            </carousel-3d>
-        </div>
-
-        <div class="category_div">
-            <h2>Nombre de la categoria</h2>
-            <carousel-3d :width="250" :height="180" :display="7" :controls-visible="true" :controls-prev-html="'❮'"
-                :controls-next-html="'❯'" :autoplay="true" :autoplay-timeout="5000" :autoplay-hover-pause="true"
-                :border="0" :disable3d="true" :space="265">
-                <slide v-for="(slide, i) in dataCarouselCategory" :index="i" :key="i"  class="slide_category">
-                    <template slot-scope="{ index, isCurrent, leftIndex, rightIndex }">
-                        <img :data-index="index"
-                            :class="{ current: isCurrent, onLeft: (leftIndex >= 0), onRight: (rightIndex >= 0) }"
-                            :src="slide.front_page" @click="navigateToMovie(slide.film_id)">
-                    </template>
-                </slide>
-            </carousel-3d>
-            <br/>
-        </div>
-        <div class="category_div">
-            <h2>Nombre de la categoria 2 </h2>
-            <carousel-3d :width="250" :height="180" :display="7" :controls-visible="true" :controls-prev-html="'❮'"
-                :controls-next-html="'❯'" :autoplay="true" :autoplay-timeout="5000" :autoplay-hover-pause="true"
-                :border="0" :disable3d="true" :space="265">
-                <slide v-for="(slide, i) in dataCarouselCategory" :index="i" :key="i" class="slide_category">
-                    <template slot-scope="{ index, isCurrent, leftIndex, rightIndex }">
-                        <img :data-index="index"
-                            :class="{ current: isCurrent, onLeft: (leftIndex >= 0), onRight: (rightIndex >= 0) }"
-                            :src="slide.front_page" @click="navigateToMovie(slide.film_id)">
-                    </template>
-                </slide>
-            </carousel-3d>
-            <br />
-        </div>
-       
+  <div class="page-container">
+    <div class="background-image-container" style="overflow:hidden;">
+      <div class="col md-12">
+        <h1 class="title">Tu mundo de películas, siempre disponible</h1>
+        <h2 class="slogan">Disfruta cuando quieras y donde quieras</h2>
+      </div>
+      <button @click="login" class="play-button">
+        Comenzar
+      </button>
     </div>
+    <div class="space">
+      <div class="row">
+        <div class="col" style="margin-top:100px; margin-left: 100px; color: white;">
+          <h1>Descubre</h1>
+          <h2 style="font-size: 30px ;">Una biblioteca cinematográfica sin límites, desde clásicos icónicos hasta las últimas novedades, todo en un solo lugar.</h2>
+        </div>
+        <div class="col" style="margin-top:20px; ;">
+          <img src="../assets/sispe.gif" width="70%" style="margin-bottom: 30px;">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col" style="margin-top:100px; margin-left: 100px; color: white;">
+          <h1>Descubre</h1>
+          <h2 style="font-size: 30px ;">Una biblioteca cinematográfica sin límites, desde clásicos icónicos hasta las últimas novedades, todo en un solo lugar.</h2>
+        </div>
+        <div class="col" style="margin-top:20px; ;">
+          <img src="../assets/sispe.gif" width="70%" style="margin-bottom: 30px;">
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
-import { Carousel3d, Slide } from 'vue-carousel-3d';
 export default {
-
-    components: {
-        Carousel3d,
-        Slide
+  methods: {
+    login() {
+      this.$router.push({name:'login'});
     },
-    data() {
-        return {
-            dataCarousel: [
-                {
-                    "film_id": "00112233445566778899aabbccddeeff",
-                    "title": "The Great Adventure",
-                    "description": "An epic journey through uncharted lands.",
-                    "length": 120.50,
-                    "status": "Activo",
-                    "fk_category": "ffddeeaabbcc99887766554433221100",
-                    "front_page": "https://cdn.vox-cdn.com/thumbor/ee_7w0oFtMc2Lse2isdoVq467z4=/0x292:2040x1360/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/16207801/jbareham_190421_0886_pikachu_0001.jpg",
-                    "file": "http://example.com/files/the_great_adventure.mp4"
-                },
-                {
-                    "film_id": "11223344556677889900aabbccddeeff",
-                    "title": "Mystery of the Lost City",
-                    "description": "A thrilling tale of ancient secrets and hidden treasures.",
-                    "length": 95.75,
-                    "status": "Activo",
-                    "fk_category": "eeddccbbaa99887766554433221100ff",
-                    "front_page": "https://www.laprensagrafica.com/__export/1505094476263/sites/prensagrafica/img/2015/08/27/7d12e526-cae2-4e36-834e-65b67cdabca1.jpg_1758632411.jpg",
-                    "file": "http://example.com/files/mystery_of_the_lost_city.mp4"
-                },
-                {
-                    "film_id": "223344556677889900aabbccddeeff00",
-                    "title": "Romance in the Rain",
-                    "description": "A love story that blossoms in the heart of a rainy city.",
-                    "length": 110.00,
-                    "status": "Inactivo",
-                    "fk_category": "ccddeeaa99887766554433221100ffbb",
-                    "front_page": "https://www.mubis.es/media/users/1864/103428/review-interstellar-christopher-nolan-2014-original.jpg",
-                    "file": "http://example.com/files/romance_in_the_rain.mp4"
-                },
-                {
-                    "film_id": "3344556677889900aabbccddeeff0011",
-                    "title": "Space Odyssey",
-                    "description": "A groundbreaking sci-fi adventure across the universe.",
-                    "length": 150.25,
-                    "status": "Activo",
-                    "fk_category": "bbaadd99887766554433221100ffeedd",
-                    "front_page": "https://elcomercio.pe/resizer/tzYPKgD1IiCQItsvPDh8NamCjKA=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/JOORGFI26BAGJIYP6NP25MVKB4.jpg",
-                    "file": "http://example.com/files/space_odyssey.mp4"
-                }
-            ],
-            dataCarouselCategory: [
-                {
-                    "film_id": "00112233445566778899aabbccddeeff",
-                    "title": "The Great Adventure",
-                    "description": "An epic journey through uncharted lands.",
-                    "length": 120.50,
-                    "status": "Activo",
-                    "fk_category": "ffddeeaabbcc99887766554433221100",
-                    "front_page": "https://img.asmedia.epimg.net/resizer/v2/5IVIDTIFJJF4FPVSPNTCL42EVE.jpg?auth=0dc9e916d1b4e5dc8362d3f9e02421b3ff523727d0d5b5b7389fdb11cb1c96e0&width=1472&height=1104&smart=true",
-                    "file": "http://example.com/files/the_great_adventure.mp4"
-                },
-                {
-                    "film_id": "11223344556677889900aabbccddeeff",
-                    "title": "Mystery of the Lost City",
-                    "description": "A thrilling tale of ancient secrets and hidden treasures.",
-                    "length": 95.75,
-                    "status": "Activo",
-                    "fk_category": "eeddccbbaa99887766554433221100ff",
-                    "front_page": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4mpt46Iuk6Pr-a4heOg1k6o3KJRGo57tE1Q&s",
-                    "file": "http://example.com/files/mystery_of_the_lost_city.mp4"
-                },
-                {
-                    "film_id": "223344556677889900aabbccddeeff00",
-                    "title": "Romance in the Rain",
-                    "description": "A love story that blossoms in the heart of a rainy city.",
-                    "length": 110.00,
-                    "status": "Inactivo",
-                    "fk_category": "ccddeeaa99887766554433221100ffbb",
-                    "front_page": "https://i.blogs.es/c7ed10/screenshot_90/1366_2000.jpeg",
-                    "file": "http://example.com/files/romance_in_the_rain.mp4"
-                },
-                {
-                    "film_id": "3344556677889900aabbccddeeff0011",
-                    "title": "Space Odyssey",
-                    "description": "A groundbreaking sci-fi adventure across the universe.",
-                    "length": 150.25,
-                    "status": "Activo",
-                    "fk_category": "bbaadd99887766554433221100ffeedd",
-                    "front_page": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/DB176BD1488D7E4822256EF1778C124FC17388FC1E7F0F6D89B38AFF5FB001F6/scale?width=1200&amp;aspectRatio=1.78&amp;format=webp",
-                    "file": "http://example.com/files/space_odyssey.mp4"
-                },
-                {
-                    "film_id": "44556677889900aabbccddeeff001122",
-                    "title": "The Secret Garden",
-                    "description": "Discover the hidden wonders of a forgotten garden.",
-                    "length": 115.75,
-                    "status": "Activo",
-                    "fk_category": "ffddeeaabbcc99887766554433221100",
-                    "front_page": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/713EAC98B3CEC6A774A84243AD1FA532576F243B8A152FEA0C03C4E32BBFE018/scale?width=1200&aspectRatio=1.78&format=webp",
-                    "file": "http://example.com/files/the_secret_garden.mp4"
-                },
-                {
-                    "film_id": "556677889900aabbccddeeff00112233",
-                    "title": "Jurassic Adventure",
-                    "description": "An expedition into the world of dinosaurs.",
-                    "length": 130.00,
-                    "status": "Activo",
-                    "fk_category": "eeddccbbaa99887766554433221100ff",
-                    "front_page": "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2023/04/keanu-reeves-saga-john-wick-3010958.jpg?tf=3840x",
-                    "file": "http://example.com/files/jurassic_adventure.mp4"
-                },
-                {
-                    "film_id": "6677889900aabbccddeeff0011223344",
-                    "title": "The Phantom Manor",
-                    "description": "Explore the eerie secrets of a haunted mansion.",
-                    "length": 105.50,
-                    "status": "Inactivo",
-                    "fk_category": "ccddeeaa99887766554433221100ffbb",
-                    "front_page": "https://todoaccion.com/wp-content/uploads/2023/09/La-Monja-2-personaje-monja-Todo-Accion-1080x675.jpg",
-                    "file": "http://example.com/files/phantom_manor.mp4"
-                },
-                {
-                    "film_id": "77889900aabbccddeeff001122334455",
-                    "title": "Underwater Odyssey",
-                    "description": "Journey to the depths of the ocean in search of lost treasures.",
-                    "length": 140.25,
-                    "status": "Activo",
-                    "fk_category": "bbaadd99887766554433221100ffeedd",
-                    "front_page": "https://arteycultura.com.mx/wp-content/uploads/2014/11/interestelar.jpg",
-                    "file": "http://example.com/files/underwater_odyssey.mp4"
-                }
-            ],
-        }
-    },
-    methods: {
-        navigateToMovie(filmId) {
-            this.$router.push({ name: 'MovieView', params: { id: filmId } });
-        }
-    }
+  },
 }
 </script>
 
 <style>
-.main-container {
-    background: #061523;
-    height: 100vh;
+.page-container {
+  position: relative;
+  z-index: 1; /* Asegura que esté delante del fondo */
+}
+.background-image-container {
+  background-image: url('/2.png');
+  background-size: cover;
+  background-position: center;
+  width: 10 0vw;
+  height: 100vh;
+  position: relative;
+  z-index: 0;
+}
+.title{
+    width: 50%;
+    margin-top: 200px;
+    margin-left: 70px;
+    font-size: 65px;
+    color: white;
+    font-weight: bold;
+}
+.slogan{
+margin-top: 50px;
+margin-left: 80px;
+font-size: 30px;
+color: rgb(255, 255, 255);
+}
+.space {
+  background-color: #00050D;
 }
 
-.category_div {
-    background: #061523;
-    color: #ffffff;
+.play-button {
+  align-items: right;
+    margin-left: 100px;
+  display: inline-block;
+  padding: 13px 30px;
+  margin-top: 100px;
+  background: #f5f5f5;
+  color: rgb(0, 0, 0);
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  font-size: 1em;
+  transition: background 0.3s;
 }
 
-#carousel_banner {
-    margin-top: -20px;
-    background: #000000;
-}
-.slide {
-    display: flex;
-}
-.slide_category{
-    display: flex;
-    border-radius:15px;
+.play-button:hover {
+  background: #c6c6c684;
+  color: #ffffff;
 }
 
+@media (max-width: 768px) {
+  .play-button {
+    top: 30%;
+    left: 5%;
+  }
+  .title{
+    margin-top: 100px;    
+    left: 1%;
+    font-size: 50px;
+    color: white;
+    font-weight: bold;
+}
+}
 </style>
-
-<!-- https://wlada.github.io/vue-carousel-3d/api/ -->
-<!-- https://madewithvuejs.com/carousels -->
-<!-- https://warpcgd.github.io/vue-concise-slider/?ref=madewithvuejs.com -->
