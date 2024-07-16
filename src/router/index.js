7a0140a8554f0e670cdd5949fe62a823a73393d3
Yexzari <20203tn141@utez.edu.mx>
2024-07-16 08:22:38 -0600

    Route Configuration and Blocking Routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,52 +1,135 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-const router = new VueRouter({
-  mode: 'history',
-  base: import.meta.env.BASE_URL,
-  
-  routes: [
-    {
-      path: '/login',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/create_account',
-      name: 'create_account',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/Create_accountView.vue')
-    },
-    {
-      path: '/',
-      component: () => import('../views/Navbar.vue'),
-      children: [
-          {
-            path: '',
-            name: 'home',
-            component: () => import('../views/LandingView.vue')
-          },
-          {
-            path: 'movie/:id',
-            name: 'MovieView',
-            component: () => import('../views/MovieView.vue')
-          },
-        ]
-      }
-  ]
-})
+const routes = [
+  {
+    path: "/",
+    redirect: "/landing",
+  },
+  {
+    path: "/unauthorized",
+    name: "unauthorized",
+    component: () => import("../views/auth/AccessDenied.vue"),
+  },
+  {
+    path: "*",
+    component: () => import("../views/auth/Error404.vue"),
+  },
+  {
+    path: "/admin",
+    component: () => import("../views/NavbarNavegation/NavbarAdmin.vue"),
+    meta: { requiresAuth: true, roles: ['ADMIN'] },
+    children: [
+      {
+        path: "categories",
+        name: "categories",
+        component: () => import("../views/admin/Categories.vue"),
+      },
+      {
+        path: "profile-admin",
+        name: "profile-admin",
+        component: () => import("../views/admin/ProfileAdmin.vue"),
+      },
+      {
+        path: "movies",
+        name: "movies",
+        component: () => import("../views/admin/Movies.vue"),
+      },
+    ],
+  },  
+  {
+    path: "/client",
+    component: () => import("../views/NavbarNavegation/NavbarUser.vue"),
+    meta: { requiresAuth: true, roles: ['CLIENT'] },
+    children: [
+      {
+        path: "",
+        name: "home",
+        component: () => import("../views/User/HomeView.vue"),
+      },
+      {
+        path: "movie/:id",
+        name: "movie-view",
+        component: () => import("../views/User/MovieView.vue"),
+      },
+      {
+        path: "profile",
+        name: "profile",
+        component: () => import("../views/User/ProfileView.vue"),
+      },
+      {
+        path: "search",
+        name: "search",
+        component: () => import("../views/User/SearchMovie.vue"),
+      },
+      {
+        path: "favorite",
+        name: "favorite",
+        component: () => import("../views/User/Favorite.vue"),
+      },
+    ],
+  },  
+  {
+    path: "/",
+    component: () => import("../views/NavbarNavegation/Navbar.vue"),  
+    children: [
+      {
+        path: "/landing",
+        name: "landing",
+        component: () => import("../views/LandingView.vue"),
+      },
+      {
+        path: "/login",
+        name: "login",
+        component: () => import("../views/auth/LoginView.vue"),
+      },
+      {
+        path: "/register",
+        name: "register",
+        component: () => import("../views/auth/Create_accountView.vue"),
+      },
+    ],
+  },
+];
 
-export default router
+const router = new VueRouter({ 
+  routes,
+  mode: 'history' 
+});
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  const storedUser = localStorage.getItem("authUser");
+  let user = null;
+
+  try {
+    user = storedUser ? JSON.parse(storedUser) : null;
+  } catch (error) {
+    console.error("Error al analizar los datos del usuario almacenado:", error);
+  }
+
+  if (requiresAuth && !user) {
+    next({ name: "unauthorized" });
+    return;
+  }
+
+  if (requiresAuth && user) {
+    const userRole = user.user.rol.nrol;
+    const requiredRoles = to.meta.roles || [];
+
+    console.log(`Rol del Usuario: ${userRole}`);
+    console.log(`Roles Requeridos para la ruta: ${requiredRoles.length > 0 ? requiredRoles.join(", ") : "Ninguno"}`);
+
+    if (requiredRoles.length > 0 && !requiredRoles.includes(userRole)) {
+      next({ name: "unauthorized" });
+      return;
+    }
+  }
+
+  next();
+});
+
+
+export default router;
