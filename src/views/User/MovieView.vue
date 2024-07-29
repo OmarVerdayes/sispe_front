4fc4602482f2d90020ea62cffa3d7c26dd7d54bc
OmarVerdayes <20213tn042@utez.edu.mx>
2024-07-28 22:20:06 -0600

    consume get categorys film by film id and get rateing by film id

--- src/views/User/MovieView.vue
+++ src/views/User/MovieView.vue
@@ -10,16 +10,14 @@
         <p id="detail">2006 · Ciencia Ficción · {{ movie.length }} min</p>
         <p style="width: 700px"><strong></strong> {{ movie.description }}</p>
         <button @click="goBack" class="play-button">
-          <b-icon icon="play-fill
-" font-scale="1.3"></b-icon>
+          <b-icon icon="play-fill" font-scale="1.3"></b-icon>
           Reproducir
         </button>
         <button @click="goBack" class="trailer-button">
           Tráiler
         </button>
         <button @click="goBack" class="favorite-button">
-          <b-icon icon="star
-" font-scale="1.3"></b-icon>
+          <b-icon icon="star" font-scale="1.3"></b-icon>
         </button>
       </div>
       <div class="Suggested" style="margin-top: 200px">
@@ -59,12 +57,17 @@
 
             <div class="container">
               <div class="row">
-                <div class="col-12 col-md-6 mb-4" v-for="comment in listComments" :key="comment.fk_user">
-                  <b-card class=" text-white" style="background-color: rgba(51, 51, 51, 0.7); border: none;">
+                <!-- Mostrar mensaje si listComments está vacío -->
+                <div v-if="listComments.length === 0" class="col-12">
+                  <p>No hay opiniones para esta película, ¡sé el primero en opinar!</p>
+                </div>
+                <!-- Mostrar comentarios si listComments tiene datos -->
+                <div v-else class="col-12 col-md-6 mb-4" v-for="comment in listComments" :key="comment.fk_user">
+                  <b-card class="text-white" style="background-color: rgba(51, 51, 51, 0.7); border: none;">
                     <b-card-text>
                       <div class="row align-items-center">
                         <div class="col-md-8">
-                          <strong>User: </strong>{{ comment.rateing_id }}
+                          <strong>User: </strong>{{ comment.email }}
                         </div>
                         <div class="col-md-4 d-flex justify-content-end">
                           <span v-for="n in 5" :key="n" style="color: gold;">
@@ -80,9 +83,6 @@
                 </div>
               </div>
             </div>
-
-
-
           </div>
         </div>
       </div>
@@ -91,141 +91,16 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
       currentRating: 0,
       stars: [1, 2, 3, 4, 5],
       movie: {},
-      suggested: [
-        {
-          film_id: "00112233445566778899aabbccddeeff",
-          title: "The Great Adventure",
-          description: "An epic journey through uncharted lands.",
-          length: 120.5,
-          status: "Activo",
-          fk_category: "ffddeeaabbcc99887766554433221100",
-          front_page:
-            "https://img.asmedia.epimg.net/resizer/v2/5IVIDTIFJJF4FPVSPNTCL42EVE.jpg?auth=0dc9e916d1b4e5dc8362d3f9e02421b3ff523727d0d5b5b7389fdb11cb1c96e0&width=1472&height=1104&smart=true",
-          file: "http://example.com/files/the_great_adventure.mp4",
-        },
-        {
-          film_id: "11223344556677889900aabbccddeeff",
-          title: "Mystery of the Lost City",
-          description:
-            "A thrilling tale of ancient secrets and hidden treasures.",
-          length: 95.75,
-          status: "Activo",
-          fk_category: "eeddccbbaa99887766554433221100ff",
-          front_page:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4mpt46Iuk6Pr-a4heOg1k6o3KJRGo57tE1Q&s",
-          file: "http://example.com/files/mystery_of_the_lost_city.mp4",
-        },
-        {
-          film_id: "223344556677889900aabbccddeeff00",
-          title: "Romance in the Rain",
-          description:
-            "A love story that blossoms in the heart of a rainy city.",
-          length: 110.0,
-          status: "Inactivo",
-          fk_category: "ccddeeaa99887766554433221100ffbb",
-          front_page: "https://i.blogs.es/c7ed10/screenshot_90/1366_2000.jpeg",
-          file: "http://example.com/files/romance_in_the_rain.mp4",
-        },
-        {
-          film_id: "3344556677889900aabbccddeeff0011",
-          title: "Space Odyssey",
-          description: "A groundbreaking sci-fi adventure across the universe.",
-          length: 150.25,
-          status: "Activo",
-          fk_category: "bbaadd99887766554433221100ffeedd",
-          front_page:
-            "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/DB176BD1488D7E4822256EF1778C124FC17388FC1E7F0F6D89B38AFF5FB001F6/scale?width=1200&amp;aspectRatio=1.78&amp;format=webp",
-          file: "http://example.com/files/space_odyssey.mp4",
-        },
-        {
-          film_id: "44556677889900aabbccddeeff001122",
-          title: "The Secret Garden",
-          description: "Discover the hidden wonders of a forgotten garden.",
-          length: 115.75,
-          status: "Activo",
-          fk_category: "ffddeeaabbcc99887766554433221100",
-          front_page:
-            "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/713EAC98B3CEC6A774A84243AD1FA532576F243B8A152FEA0C03C4E32BBFE018/scale?width=1200&aspectRatio=1.78&format=webp",
-          file: "http://example.com/files/the_secret_garden.mp4",
-        },
-        {
-          film_id: "556677889900aabbccddeeff00112233",
-          title: "Jurassic Adventure",
-          description: "An expedition into the world of dinosaurs.",
-          length: 130.0,
-          status: "Activo",
-          fk_category: "eeddccbbaa99887766554433221100ff",
-          front_page:
-            "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2023/04/keanu-reeves-saga-john-wick-3010958.jpg?tf=3840x",
-          file: "http://example.com/files/jurassic_adventure.mp4",
-        },
-        {
-          film_id: "6677889900aabbccddeeff0011223344",
-          title: "The Phantom Manor",
-          description: "Explore the eerie secrets of a haunted mansion.",
-          length: 105.5,
-          status: "Inactivo",
-          fk_category: "ccddeeaa99887766554433221100ffbb",
-          front_page:
-            "https://todoaccion.com/wp-content/uploads/2023/09/La-Monja-2-personaje-monja-Todo-Accion-1080x675.jpg",
-          file: "http://example.com/files/phantom_manor.mp4",
-        },
-        {
-          film_id: "77889900aabbccddeeff001122334455",
-          title: "Underwater Odyssey",
-          description:
-            "Journey to the depths of the ocean in search of lost treasures.",
-          length: 140.25,
-          status: "Activo",
-          fk_category: "bbaadd99887766554433221100ffeedd",
-          front_page:
-            "https://arteycultura.com.mx/wp-content/uploads/2014/11/interestelar.jpg",
-          file: "http://example.com/files/underwater_odyssey.mp4",
-        },
-      ],
-      listComments: [
-        {
-          "rateing_id": "e8b08f9b50b141c7b3a30720a2a5b7a1",
-          "grade": 4.5,
-          "comment": "¡Gran película, realmente la disfruté!",
-          "fk_user": "12c1af3b91e14184a4a8cfc2b89f1ed9",
-          "fk_film": "53c2ff1b61e14523b9e7b762d6d9a8f1"
-        },
-        {
-          "rateing_id": "d4c4f9bb90e7417cb2c62d22a4b4b3d3",
-          "grade": 3.2,
-          "comment": "Estuvo bien, podría haber sido mejor.",
-          "fk_user": "a6b2df4e83d84283a4a5b0b1c4f2d4c7",
-          "fk_film": "74d3ee2c53d84523b8d9c7d7e6e1a9e2"
-        },
-        {
-          "rateing_id": "f1a3d8e9c1b44172a4e7d7b4c3a5b6a7",
-          "grade": 5.0,
-          "comment": "¡Absolutamente fantástica! Imprescindible.",
-          "fk_user": "b3e2d9f4a1c94163a5d7b7d7d6d2b1f3",
-          "fk_film": "e4a2b9d1f3e64528b8a7c7f7d6d1e2a3"
-        },
-        {
-          "rateing_id": "a2e3f4d5c1b2417eb8a6b2b1c4d3d4e5",
-          "grade": 2.8,
-          "comment": "No es mi estilo.",
-          "fk_user": "c2b1d7f4e3a84175a4d2b2c3d4e3f1b2",
-          "fk_film": "d3c2e4b5a1f74268b6c7d7f7d6e2e3c1"
-        },
-        {
-          "rateing_id": "c5d4e2a1b4c74179b3d6b2a1e3f2d4a5",
-          "grade": 4.0,
-          "comment": "Muy entretenida y bien hecha.",
-          "fk_user": "d1e2c3f4b3a74167a5d2c3e4d1f3a2b3",
-          "fk_film": "c6b5d7e3a2e84769b5d6e7f7d6d1c2b4"
-        }
-      ]
+      suggested: [],
+      listComments: []
     };
   },
   created() {
@@ -243,103 +118,38 @@ export default {
       this.currentRating = rating;
       this.$emit('rated', rating);
     },
-    loadMovie() {
+    async loadMovie() {
       const filmId = this.$route.params.id;
-      const movies = [
-        {
-          film_id: "00112233445566778899aabbccddeeff",
-          title: "The Great Adventure",
-          description: "An epic journey through uncharted lands.",
-          length: 120.5,
-          status: "Activo",
-          fk_category: "ffddeeaabbcc99887766554433221100",
-          front_page:
-            "https://img.asmedia.epimg.net/resizer/v2/5IVIDTIFJJF4FPVSPNTCL42EVE.jpg?auth=0dc9e916d1b4e5dc8362d3f9e02421b3ff523727d0d5b5b7389fdb11cb1c96e0&width=1472&height=1104&smart=true",
-          file: "http://example.com/files/the_great_adventure.mp4",
-        },
-        {
-          film_id: "11223344556677889900aabbccddeeff",
-          title: "Entrenando a mi dragon",
-          description:
-            "A thrilling tale of ancient secrets and hidden treasures.",
-          length: 95.75,
-          status: "Activo",
-          fk_category: "eeddccbbaa99887766554433221100ff",
-          front_page:
-            "https://media.glamour.mx/photos/6477bfe1af12f5514d139ef3/16:9/w_1600,c_limit/IMG_6625.jpeg",
-          file: "http://example.com/files/mystery_of_the_lost_city.mp4",
-        },
-        {
-          film_id: "223344556677889900aabbccddeeff00",
-          title: "Spider-Man",
-          description:
-            " La historia sigue a Peter Parker, un estudiante de secundaria que adquiere poderes arácnidos tras ser picado por una araña radiactiva. ",
-          length: 110.0,
-          status: "Inactivo",
-          fk_category: "ccddeeaa99887766554433221100ffbb",
-          front_page: "https://i.blogs.es/c7ed10/screenshot_90/1366_2000.jpeg",
-          file: "http://example.com/files/romance_in_the_rain.mp4",
-        },
-        {
-          film_id: "3344556677889900aabbccddeeff0011",
-          title: "Avengers - EndGame",
-          description: "La trama sigue a los Vengadores restantes mientras intentan revertir el daño causado por Thanos, quien erradicó a la mitad del universo al chasquear sus dedos con las Gemas del Infinito en Avengers: Infinity War",
-          length: 150.25,
-          status: "Activo",
-          fk_category: "bbaadd99887766554433221100ffeedd",
-          front_page:
-            "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/DB176BD1488D7E4822256EF1778C124FC17388FC1E7F0F6D89B38AFF5FB001F6/scale?width=1200&amp;aspectRatio=1.78&amp;format=webp",
-          file: "http://example.com/files/space_odyssey.mp4",
-        },
-        {
-          film_id: "44556677889900aabbccddeeff001122",
-          title: "Avatar",
-          description: "Ambientada en el planeta Pandora, sigue a Jake Sully, un exmarine parapléjico que participa en un programa que lo conecta con un avatar Na'vi, una raza indígena humanoides",
-          length: 115.75,
-          status: "Activo",
-          fk_category: "ffddeeaabbcc99887766554433221100",
-          front_page:
-            "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/713EAC98B3CEC6A774A84243AD1FA532576F243B8A152FEA0C03C4E32BBFE018/scale?width=1200&aspectRatio=1.78&format=webp",
-          file: "http://example.com/files/the_secret_garden.mp4",
-        },
-        {
-          film_id: "556677889900aabbccddeeff00112233",
-          title: "John Wick",
-          description: "Wick, devastado por la pérdida de su esposa, se ve obligado a regresar al mundo del crimen después de que un grupo de gángsters rusos roba su automóvil y mata a su perro, el último regalo de su difunta esposa",
-          length: 130.0,
-          status: "Activo",
-          fk_category: "eeddccbbaa99887766554433221100ff",
-          front_page:
-            "https://mx.web.img3.acsta.net/r_654_368/newsv7/19/05/16/21/51/0744636.jpg",
-          file: "http://example.com/files/jurassic_adventure.mp4",
-        },
-        {
-          film_id: "6677889900aabbccddeeff0011223344",
-          title: "La Monja",
-          description:
-            "Un sacerdote y una novicia enviados por el Vaticano para investigar el suicidio de una monja en un convento en Rumania en 1952. Descubren secretos oscuros y se enfrentan al demonio Valak, que adopta la forma de una monja para aterrorizarlos.",
-          length: 105.5,
-          status: "Inactivo",
-          fk_category: "ccddeeaa99887766554433221100ffbb",
-          front_page:
-            "https://todoaccion.com/wp-content/uploads/2023/09/La-Monja-2-personaje-monja-Todo-Accion-1080x675.jpg",
-          file: "http://example.com/files/phantom_manor.mp4",
-        },
-        {
-          film_id: "77889900aabbccddeeff001122334455",
-          title: "Interestelar",
-          description:
-            "Un grupo de exploradores que viajan a través de un agujero de gusano en busca de un nuevo hogar para la humanidad, amenazada por la escasez de recursos en la Tierra. ",
-          length: 140.25,
-          status: "Activo",
-          fk_category: "bbaadd99887766554433221100ffeedd",
-          front_page:
-            "https://arteycultura.com.mx/wp-content/uploads/2014/11/interestelar.jpg",
-          file: "http://example.com/files/underwater_odyssey.mp4",
-        },
 
-      ];
-      this.movie = movies.find((movie) => movie.film_id === filmId) || {};
+      try {
+        const response = await axios.get(`https://djpg1wqrj2.execute-api.us-east-1.amazonaws.com/Prod/get_category_films_by_film_id/${filmId}`);
+        const data = response.data;
+
+        this.movie = data.find((movie) => movie.film_id === filmId) || {};
+
+        // Filtrar la película actual para que no esté en la lista sugerida
+        let lista = data.filter(movie => movie.film_id !== filmId);
+
+
+        const filteredMovies = data.filter((movieI) => movieI.film_id !== filmId);
+        this.suggested = filteredMovies;
+
+        console.log(this.suggested)
+
+
+      } catch (error) {
+        console.error('Error al cargar la película:', error);
+      }
+      try {
+        const commentsResponse = await axios.get(`https://1yasnjn821.execute-api.us-east-1.amazonaws.com/Prod/get_rateing_by_film_id/${filmId}`);
+        if (commentsResponse.data === "No rateings found for the given film") {
+          this.listComments = []; // No hay comentarios
+        } else {
+          this.listComments = commentsResponse.data;
+        }
+      } catch (error) {
+        console.error('Error al cargar los rateings:', error);
+      }
     },
     goBack() {
       this.$router.push({ name: "home" });
